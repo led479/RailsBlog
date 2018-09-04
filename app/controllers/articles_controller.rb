794f062de0de3e494386b777f075afe2614c3152
Rolf Zambon <led479.rs@gmail.com>
2018-09-04 17:22:49 -0300

    Agora apresenta mensagem de erro se o título é inválido

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,11 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-
     if @article.save
       redirect_to @article
     else
